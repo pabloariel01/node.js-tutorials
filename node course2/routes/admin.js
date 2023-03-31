80d019b89d9cb4b496ce8530a3238cb97b157914
Pablo Silva <pablo.silva@dataart.com>
2023-03-31 18:03:13 -0300

    added pug

--- node course2/routes/admin.js
+++ node course2/routes/admin.js
@@ -5,13 +5,18 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+//   res.sendFile(path.join(rootDir, "views", "add-product.html"));
+res.render('add-product', {pageTitle: 'Add Products'})
 });
 
 router.post("/add-product", (req, res) => {
-  console.log(req.body);
+  console.log("adminjs", req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
