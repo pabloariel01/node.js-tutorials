d72563768adbc8bc841f54636adb8a88ccee07a6
Pablo Silva <pablo.silva@dataart.com>
2023-03-31 13:53:23 -0300

    added express router

--- node course2/app.js
+++ node course2/app.js
@@ -1,12 +1,18 @@
-const http = require("http");
-
 const express = require("express");
-const { start } = require("repl");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.send("<h1>Hello</h1>");
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
