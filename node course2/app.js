754322b296017684b889d26a291c8b532b801297
Pablo Silva <pablo.silva@dataart.com>
2023-04-03 13:36:18 -0300

    enhanced proyect

--- node course2/app.js
+++ node course2/app.js
@@ -1,35 +1,23 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-// const { engine } = require("express-handlebars");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-//for handlebars
-// app.engine(
-//   "hbs",
-//   engine({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
-// app.set("view engine", "pug"); //for pug
-// app.set("view engine", "hbs"); //for handlebars
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/404");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 
 app.use(errorController.get404);
 
--- node course2/models/product.js
+++ node course2/models/product.js
@@ -1,29 +1,36 @@
-const fs = require("fs");
-const path = require("path");
-const basePath = require("../util/path");
-const p = path.join(basePath, "data", "products.json");
+const fs = require('fs');
+const path = require('path');
 
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContet) => {
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
     } else {
-      cb(JSON.parse(fileContet));
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
